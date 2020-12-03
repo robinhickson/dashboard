f600cd20f2716dae5fd187826aca9b0d365a1e1f
robinhickson <rkhickson@gmail.com>
2020-12-03 22:22:15 +0100

    Init

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+//Project 7 Web Dashboard
+
+
+import {webTrafficChartWeekly, webTrafficChartDaily, webTrafficChartMonthly, webTrafficChartHourly, dailyTrafficBarChart, mobileUsersDonutChart} from './charts.js';
+
+//Line charts - traffic
+webTrafficChartWeekly(); 
+// webTrafficChartDaily();
+// webTrafficChartMonthly();
+// webTrafficChartHourly();
+
+//Bar chart - daily traffic
+dailyTrafficBarChart();
+
+//Donut chart - mobile users
+mobileUsersDonutChart();
+
