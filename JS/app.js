6c4c3212754b4daaa52dea6c07c7b7a7e30a295a
robinhickson <rkhickson@gmail.com>
2020-12-04 10:20:08 +0100

    v4

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,30 @@
 //Project 7 Web Dashboard
 
 
+/* ----------  INTERACTIVITY --------
+--------------------------------------------*/
+
+const closeAlert = () =>{
+    const getAlertArea = document.querySelector('.alertArea');
+    getAlertArea.addEventListener('click', (e) => {
+        
+        if(e.target.className === 'closeIcon'){
+            if(getAlertArea.style.display === "flex"){
+                getAlertArea.style.display="none";
+            }
+            else{
+                getAlertArea.style.display="flex";    
+            }
+        }
+    });
+};
+
+closeAlert();
+
+
+/* ----------  CHART AREA - CHART.JS --------
+--------------------------------------------*/
+
 import {webTrafficChartWeekly, webTrafficChartDaily, webTrafficChartMonthly, webTrafficChartHourly, dailyTrafficBarChart, mobileUsersDonutChart} from './charts.js';
 
 //Line charts - traffic
