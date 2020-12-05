391c15b799184c06c7d53b27e68851f6627e83d7
robinhickson <rkhickson@gmail.com>
2020-12-05 10:02:49 +0100

    V12

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@
 /* ----------  INTERACTIVITY --------
 --------------------------------------------*/
 
-const getListener = document.querySelector('.gridContainer');
+const getListener = document.querySelector('body');
 const getAlertArea = document.querySelector('.alertArea');
 getListener.addEventListener('click', (e) => {
     if (e.target.className === 'closeIcon') {
@@ -23,35 +23,55 @@ const allMembers=[
         avatar:'images/member-2.jpg',
         email:'mike@klaas.au',
         joinDate:'15/04/2016',
-        status: 'Old Hand'
+        status: 'Old Hand',
+        activity: 'Recent',
+        lastMessage: 'Well, it\'s really not that difficult!',
+        lastActive: '1 hour ago',
+        profileLink: '<a href="#">\></a>',
     },
     {
         name:'Klaus Akt',
         avatar:'images/member-4.jpg',
         email:'akt@tion.ing',
         joinDate:'01/12/2017',
-        status: 'Old Hand'
+        status: 'Old Hand',
+        activity: 'Old',
+        lastMessage: 'I remember, when I started coding, we didn\'t even have electricity.',
+        lastActive: '32 hours ago',
+        profileLink: '<a href="#">\></a>',
     },
     {
         name:'Eillean Inn',
         avatar:'images/member-1.jpg',
         email:'eilleaninn@gamble.com',
         joinDate:'23/10/2020',
-        status: 'Newbie'
+        status: 'Newbie',
+        activity: 'Recent',
+        lastMessage: 'Everything you say and do is just so yesterday.',
+        lastActive: '4 hours ago',
+        profileLink: '<a href="#">\></a>',
     },
     {
         name:'Kev Lars',
         avatar:'images/member-5.jpg',
         email:'kev.lars@bulletproof.ge',
         joinDate:'01/12/2020',
-        status: 'Newbie'
+        status: 'Newbie',
+        activity: 'Recent',
+        lastMessage: 'Is this chess club?',
+        lastActive: '8 hours ago',
+        profileLink: '<a href="#">\></a>',
     },
     {
         name:'Donna Gnoe',
         avatar:'images/member-3.jpg',
         email:'gnoe@nothing.org',
         joinDate:'30/09/2019',
-        status: 'Newbie'
+        status: 'Newbie',
+        activity: 'Recent',
+        lastMessage: 'I just don\'t understand....anything.',
+        lastActive: '6 hours ago',
+        profileLink: '<a href="#">\></a>',
     },
 ];
 
@@ -70,6 +90,22 @@ for (let i=0; i<allMembers.length; i++){
    
     };
 
+/* ----------  Recent Activity widget --------
+--------------------------------------------*/  
+
+let recentActivityText = document.querySelector('.recentActivity');
+for (let i=0; i<allMembers.length; i++){
+    if (allMembers[i].activity === 'Recent'){
+        let recentActivityEntry = allMembers[i];    
+        recentActivityText.innerHTML += `<div class="aRecentActivity"><img class="recentMemberAvatar" src="${recentActivityEntry.avatar}" alt="avatar">
+        <div class="recentActivityNameActivity">
+        <h3>${recentActivityEntry.name}</h3><h4>${recentActivityEntry.lastMessage}</h4><h4>${recentActivityEntry.lastActive}</h4>
+        </div>
+        <h3 class="profileLink">${recentActivityEntry.profileLink}</h3>
+        </div>`;
+           }
+   
+    };
 
 
 
